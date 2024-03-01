2f20946416cd7e34d86716b4ec15d095e15ea6a8
jsirugo <jsirugo@yahoo.com>
2024-03-01 11:03:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+    methods: {
+        //metod för att räkna cirkel
+    },
+    computed: {
+
+    },
+    data() {
+        return {
+
+            //maträtter 
+
+        };
+    }
+}).mount('#app');
