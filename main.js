1ea808245f01769ec6adeabf954fb9b02fada41f
jsirugo <jsirugo@yahoo.com>
2024-03-01 11:59:00 +0100

    send it

--- main.js
+++ main.js
@@ -7,8 +7,67 @@ Vue.createApp({
     },
     data() {
         return {
-
-            //maträtter 
+            title: "Euclidian foods",
+            apetiser:  [
+                {
+                    meal: 'Bruschetta',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Chark',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Teriyaki pineapple meatballs',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+            ],
+            mainCourse:  [
+                {
+                    meal: 'Pizza',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Palt',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Pasta',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+            ],
+            deserts:  [
+                {
+                    meal: 'Creme',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Whole plum in champagne',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+                {
+                    meal: 'Gelato',
+                    carbs: 2,
+                    protein: 5,
+                    sugar: 10
+                },
+            ]
 
         };
     }
